738c65bd9f58bf0f34e924f9f604658272f97e17
shubham sony <88088450+shubhamsony@users.noreply.github.com>
2023-03-29 12:17:12 -0700

    Add files via upload

---
+++ posnev.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int number = scanner.nextInt();
+    if (number > 0) {
+      System.out.println("The number is positive");
+    } else {
+      System.out.println("The number is negative");
+    }
+  }
+}
